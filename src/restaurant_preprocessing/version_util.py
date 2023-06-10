1305f036a57d38b142058c74a18021de99517539
enriquebarba97 <35747594+enriquebarba97@users.noreply.github.com>
2023-06-10 23:50:05 +0200

    Include pylint workflow and fixes

--- src/restaurant_preprocessing/version_util.py
+++ src/restaurant_preprocessing/version_util.py
@@ -1,8 +1,12 @@
+"""Util to get metadata"""
 from importlib import metadata
 
 class VersionUtil:
+    """Class to obtain current library version"""
+
     @classmethod
     def get_version(cls):
+        """Function to obtain current library version"""
         try:
             return metadata.version('remla23-team10-preprocessing')
         except metadata.PackageNotFoundError:
